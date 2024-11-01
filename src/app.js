fb773bee761d42a8d5878ea97f6a810cd51f4ed7
mforget <mf@teeptrak.com>
2024-11-01 12:02:59 +0100

    Add Mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-const path = require('path');
+require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.listen(3000);
+// routes
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('listening on port', process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
