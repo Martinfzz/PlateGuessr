aca0d76101559c8bfb32981b2a444906ed4f7a64
mforget <mf@teeptrak.com>
2024-11-04 15:29:05 +0100

    Add Mapbox map

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
+import Mapbox from "./components/Map/Mapbox";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Mapbox/>
       </header>
     </div>
   );
