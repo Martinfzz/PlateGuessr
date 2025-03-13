e2e4b12c97d9131da7ae59991213a17f2de5ef2e
mforget <mf@teeptrak.com>
2025-03-13 16:18:41 +0100

    Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ export default [
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-expressions": "warn",
+      "react/display-name": "warn",
     },
   },
 ];
